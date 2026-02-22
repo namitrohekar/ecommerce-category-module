f79a066c378e849e3248ee810a740248ccf83285
Namit Rohekar <namit.rohekar.dev@gmail.com>
2026-02-22 11:52:42 +0530

    chore : base configurations for deployement

--- category-backend/src/main/java/com/namit/categorybackend/config/WebConfig.java
+++ category-backend/src/main/java/com/namit/categorybackend/config/WebConfig.java
@@ -8,15 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry){
+    public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/api/**")
                 .allowedOrigins(
-                        "http://localhost:5173"
-                )
+                        "http://localhost:5173")
                 .allowedMethods(
-                 "GET","POST","PUT","DELETE","OPTIONS"
-                )
+                        "GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
